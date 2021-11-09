204a6fc99e5f46fe04933c9b5ff0241335172ceb
alex-clements <ahclemen@ualberta.ca>
2021-11-08 20:42:20 -0800

    Updated the ReadMe file

--- src/hooks/ShutdownScreen/ShutdownScreen.js
+++ src/hooks/ShutdownScreen/ShutdownScreen.js
@@ -7,7 +7,11 @@ export default function ShutdownScreen(props) {
         <div className='shutdown-screen'>
             <div className='shutdown-screen-spacer'></div>
             <p className="shutdown-screen-text">thanks for checking this out</p>
-            <p className="shutdown-screen-text">to see more of my projects, check out my <strong><a className="shutdown-screen-text" href="https://github.com/alex-clements">GitHub</a></strong></p>
+            <p className="shutdown-screen-text">to see more of my projects, check out my 
+                <strong>
+                    <a className="shutdown-screen-text" href="https://github.com/alex-clements">GitHub</a>
+                </strong>
+            </p>
 
 
         </div>

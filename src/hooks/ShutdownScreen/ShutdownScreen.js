49b6ef1f838a4f7640c4fe461e651ac991b19f9a
alex-clements <ahclemen@ualberta.ca>
2021-11-08 20:52:21 -0800

    updated formatting on shutdown screen

--- src/hooks/ShutdownScreen/ShutdownScreen.js
+++ src/hooks/ShutdownScreen/ShutdownScreen.js
@@ -6,14 +6,8 @@ export default function ShutdownScreen(props) {
     return (
         <div className='shutdown-screen'>
             <div className='shutdown-screen-spacer'></div>
-            <p className="shutdown-screen-text">thanks for checking this out</p>
-            <p className="shutdown-screen-text">to see more of my projects, check out my 
-                <strong>
-                    <a className="shutdown-screen-text" href="https://github.com/alex-clements">GitHub</a>
-                </strong>
-            </p>
-
-
+            <p className="shutdown-screen-text">thanks for visiting.</p>
+            <p className="shutdown-screen-text">to see more of my projects, check out my <strong><a className="shutdown-screen-text" href="https://github.com/alex-clements">GitHub</a></strong>.</p>
         </div>
     )
 }

b57a3359758312cfefd6b1f8608dfe2d1274a253
alex-clements <ahclemen@ualberta.ca>
2021-11-09 10:45:06 -0800

    Added some logging changes

--- src/scripts/logData.js
+++ src/scripts/logData.js
@@ -3,23 +3,24 @@ export default function captureAnalytics(stringArg) {
         method: 'POST',
         headers: { 
             "accepts":"application/json",
-            'Content-Type': 'application/json' },
+            'Content-Type': 'application/json'},
+        mode: 'no-cors',
         body: JSON.stringify({ title: stringArg })
     };
 
-    // fetch("/api", requestOptions)
-    // .then(response => {
-    //     if (!response.ok) {
-    //         throw new Error("ERROR");
-    //     } else {
-    //         return response.json();
-    //     }
-    // })
-    // .then((responseJson) => {
-    //     console.log(responseJson)
-    // })
-    // .catch((error) => {
-    //     console.log(error);
-    // })
+    fetch("http://ec2-35-166-10-180.us-west-2.compute.amazonaws.com:5000/api", requestOptions)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("ERROR");
+        } else {
+            return response.json();
+        }
+    })
+    .then((responseJson) => {
+        console.log(responseJson)
+    })
+    .catch((error) => {
+        //pass
+    })
 
 }

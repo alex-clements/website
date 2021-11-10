b310b725bad3d185d21a966560f4b23058f4a94e
alex-clements <ahclemen@ualberta.ca>
2021-11-10 00:52:55 -0800

    fixed shutdown screen

--- src/hooks/ShutdownScreen/ShutdownScreen.js
+++ src/hooks/ShutdownScreen/ShutdownScreen.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Shutdown.css';
 import captureAnalytics from '../../scripts/captureAnalytics.js';
-import { useEffect } from 'react/cjs/react.development';
 
 export default function ShutdownScreen(props) {
 

c559d124855c2f106fb107d7525a5d5c3d2ab136
alex-clements <ahclemen@ualberta.ca>
2021-11-10 01:02:48 -0800

    fixed broken resume import

--- src/hooks/resumeFile/ResumeContents.js
+++ src/hooks/resumeFile/ResumeContents.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './ResumeFile.css';
 import captureAnalytics from '../../scripts/captureAnalytics.js';
-import { useEffect } from 'react/cjs/react.development';
 
 export default function ResumeContents(props) {
 

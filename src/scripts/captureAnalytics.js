1dec7d12dcf46f0c84528ada7b9708475a66d676
alex-clements <ahclemen@ualberta.ca>
2023-04-08 11:44:44 -0700

    Disabled analytics

--- src/scripts/captureAnalytics.js
+++ src/scripts/captureAnalytics.js
@@ -1,26 +1,30 @@
-export default function captureAnalytics(stringArg) {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      accepts: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title: stringArg }),
-  };
+const analyticsEnabled = false;
 
-  fetch("https://www.alexsapiserver.com/", requestOptions)
-    .then((response) => {
-      if (!response.ok) {
-        console.log("response not ok");
-        throw new Error("ERROR");
-      } else {
-        return response.json();
-      }
-    })
-    .then((responseJson) => {
-      //console.log(responseJson)
-    })
-    .catch((error) => {
-      //pass
-    });
+export default function captureAnalytics(stringArg) {
+  if (analyticsEnabled) {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: stringArg }),
+    };
+
+    fetch("https://www.alexsapiserver.com/", requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          console.log("response not ok");
+          throw new Error("ERROR");
+        } else {
+          return response.json();
+        }
+      })
+      .then((responseJson) => {
+        //console.log(responseJson)
+      })
+      .catch((error) => {
+        //pass
+      });
+  }
 }
